35cc88124c420d13319580d9b849ab79586953c5
Rachid ABBAD <rachid132abbad@gmail.com>
2022-09-17 12:13:27 +0100

    Reinstall

--- src/app/services/globalPath.ts
+++
@@ -1,3 +0,0 @@
-export class Globalpath{
-    static global_path = "https://admin.smartonviatoile.com";
-}
--- src/app/services/temp/gobalpath.ts
+++
@@ -1,7 +0,0 @@
-export var globalpath =
-{
-    'serverpath' : "https://admin.smartonviatoile.com/api",
-    'localhost' : "http://localhost:53816/api",
-     'backup'  : "https://admin.smartonviatoile.com/api"
-
-}
