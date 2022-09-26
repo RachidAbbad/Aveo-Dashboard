2846730371477230b82930894fa09024e2e0aae9
Rachid ABBAD <rachid132abbad@gmail.com>
2022-09-26 23:29:58 +0100

    Settings Section Done

--- src/app/parametres/parametres.component.ts
+++ src/app/parametres/parametres.component.ts
@@ -3,47 +3,79 @@ import { faSave } from '@fortawesome/free-solid-svg-icons';
 import { element } from 'protractor';
 import { serialize } from 'v8';
 import { UserService } from '../services/user.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-parametres',
   templateUrl: './parametres.component.html',
-  styleUrls: ['./parametres.component.scss']
+  styleUrls: ['./parametres.component.scss'],
 })
 export class ParametresComponent implements OnInit {
   save = faSave;
-  periodicities:Array<periodicity> = new Array<periodicity>();
-  PeriodicitiesSel: Array<any> = new Array<any>();
-  form: FormGroup;
+  periodicities: Array<periodicity> = new Array<periodicity>();
+  periodicitiesUser: Array<any> = new Array<any>();
+  settingsForm: FormGroup;
   submitted = false;
-  private formBuilder: FormBuilder
-  
-  constructor(private ser:UserService) { }
+  buttonStat = true;
+  settings = new FormGroup({});
+
+  constructor(private ser: UserService, private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-    this.ser.getPeriodicities().forEach(element=>this.periodicities.push(element));
+    //Init Parameters Form Controls
+    this.settingsForm = new FormGroup({
+      periodesCheckBoxs : new FormGroup({})
+    });
+    //Get All Periodecities
+    this.ser.getPeriodicities().forEach((element) => {
+      this.periodicities.push(element);
+    });
 
-    this.ser.getUserPeriodicities().subscribe((result : any)=>
-  {
-    let data= result.data;
-    data.forEach((element:any)=>{
-        this.PeriodicitiesSel.push(element);
-    })
+    this.ser.getUserPeriodicities().subscribe((result: any) => {
+      let data = result.data;
+      console.log(data);
+      data.forEach((element: any) => {
+        this.periodicitiesUser.push(element);
+      });
+      this.getPeriodecitiesCheckBoxs(this.settingsForm.get('periodesCheckBoxs') as FormGroup);
+    });
     
   }
 
-)
+  getPeriodecitiesCheckBoxs(formGroup:FormGroup) {
+    this.periodicities.forEach((element) => {
+      formGroup.addControl(element.value, new FormControl(this.periodicitiesUser.filter(e => e.value === element.value).length > 0));
+    });
+  }
 
+  onSubmit(form:any){
+    this.submitted = true;
+    console.log(form.value);
+    //Save Periodicities Preferred:
+    let periodes = new Array<string>();
+    this.periodicities.forEach((element, index)=>{
+      if(form.value.periodesCheckBoxs[element.value]) 
+        periodes.push(element.id)
+    })
+    this.ser.saveUserPeriodicities(periodes).subscribe((result)=>{
+      this.buttonStat = true;
+    });
 
   }
 
-  
-
+  changeCheck(control, event){
+    control = this.settingsForm.get('periodesCheckBoxs').get(control).setValue(event.target.checked);
+    this.buttonStat = false;
+  }
 }
 
-interface periodicity{
-  id:string;
-  value:string
+interface periodicity {
+  id: string;
+  value: string;
 }
-
